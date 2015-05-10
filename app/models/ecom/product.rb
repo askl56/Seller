49a834729bf345f502beb2287b6716174a309a17
askl56 <amscott@openmailbox.org>
2015-05-10 18:32:55 +0100

    fixed db issues

---
+++ app/models/ecom/product.rb
@@ -0,0 +1,11 @@
+module Ecom
+  class Product
+    include Mongoid::Document
+    include Mongoid::Slug
+    field :name, type: String
+    field :description, type: String
+    field :base_price, type: Float
+    field :sku, type: String
+    slug :name, history: true
+  end
+end
---
+++ test/models/ecom/product_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+module Ecom
+  class ProductTest < ActiveSupport::TestCase
+    # test "the truth" do
+    #   assert true
+    # end
+  end
+end
